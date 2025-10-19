ece2752d3d54affd552030b7699d7f1bc59a85e0
Omar Ahmed <omarzaafan732@gmail.com>
2025-10-19 19:17:49 +0300

    fix again

--- hyperland_prerequisite.sh
+++ hyperland_prerequisite.sh
@@ -2,24 +2,45 @@
 
 # Install build dependencies
 sudo apt install -y \
-  build-essential \
-  git \
-  cmake \
-  meson \
-  ninja-build \
-  pkg-config \
+  libwlroots-dev \
   libwayland-dev \
+  wayland-protocols \
   libxkbcommon-dev \
   libinput-dev \
   libpixman-1-dev \
   libgbm-dev \
   libegl-dev \
-  libgles2 \
+  libgles2-mesa-dev \
   libvulkan-dev \
-  libx11-dev \
+  libdrm-dev \
+  libliftoff-dev \
+  libhwf-dev \
   libxcb1-dev \
-  libxcb-randr0-dev
+  libxcb-composite0-dev \
+  libxcb-xinput-dev \
+  libxcb-randr0-dev \
+  libxcb-icccm4-dev \
+  libxcb-xfixes0-dev
 
+# Install aquamarine dependencies
+sudo apt install -y \
+  libliftoff-dev \
+  libhwf-dev \
+  libdrm-dev
+
+# Clone and build aquamarine
+git clone https://github.com/hyprwm/aquamarine
+cd aquamarine
+meson build
+ninja -C build
+sudo ninja -C build install
+cd ..
+
+# Now try building Hyprland again
+cd Hyprland
+meson build
+ninja -C build
+sudo ninja -C build install
 
 # Clone and build Hyprland
 git clone https://github.com/hyprwm/Hyprland
